cee8776594d4e9967c5364acde60359e53ffc0e9
Alex Tsai <alex.tsai@pdabc.com>
2021-04-08 14:36:18 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"os"
+
+	"github.com/net-byte/opensocks/config"
+	"github.com/net-byte/opensocks/server"
+)
+
+func main() {
+	port := os.Getenv("_PORT")
+	if port == "" {
+		port = "8080"
+	}
+	username := os.Getenv("_USERNAME")
+	if username == "" {
+		username = "admin"
+	}
+	password := os.Getenv("_PASSWORD")
+	if password == "" {
+		password = "pass@123456"
+	}
+	config := config.Config{}
+	config.ServerMode = true
+	config.ServerAddr = ":" + port
+	config.Username = username
+	config.Password = password
+	config.Init()
+	server.Start(config)
+}
